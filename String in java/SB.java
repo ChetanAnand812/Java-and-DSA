151d46f31710b1da42214ccbd0efc7fe13c80242
ChetanAnand812 <chetananand812@gmail.com>
2022-05-20 22:27:11 +0530

    initial commit

--- String in java/SB.java
+++ String in java/SB.java
@@ -1,7 +1,7 @@
 public class SB {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Tony");
-        System.out.println(sb);
+       StringBuilder sb = new StringBuilder("Tony");
+       System.out.println(sb);
 
         // char at index 0
         System.out.println(sb.charAt(0));
@@ -10,7 +10,7 @@ public class SB {
         sb.setCharAt(0, 'P');
         System.out.println(sb);
 
-        // insert extra 'n'
+        // insert extra 'n' at index 2
         sb.insert(2, 'n');
         System.out.println(sb);
 
